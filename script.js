600f21e32cf68031876c5696e2ce977d11237731
gopikantagarain <garaingopi941@gmail.com>
2023-06-24 14:04:16 +0530

    the weather api

---
+++ script.js
@@ -0,0 +1,46 @@
+// class car{
+//     age=30
+//     adult=true
+//     constructor(brand,name){
+//         this.name=name
+//         this.brand=brand
+//         this.age=22
+//     }
+//     greet(){
+//         console.log('hello '+this.name+ ' your age is '+this.age)
+//     }
+// }
+// const car1=new car('marsedes','audi')
+// car1.greet()
+// function bike(brand,name){
+//     this.name=name
+//     this.brand=brand
+// }
+// const bike1=new bike('ninja','zx10r')
+
+// console.log(bike1)
+// const obj={
+//     name:'Gopi'
+// }
+// const obj2=obj
+// obj.name='Gagan'
+// console.log(obj)
+// let a=3
+// let b=a
+// a+=25
+// console.log(a)
+// console.log(b)
+//const arr =['Gopi','Gagan','Rahul','Puchu']
+// console.log(arr.push(11))
+// console.log(arr.unshift(0))
+// arr.pop()
+// console.log(arr)
+//foreach,map,filter,find,reduce,findindex
+//const reducefunction=function(previousvalue.currentvalue)
+const greet= function(){
+    console.log('hello!')
+}
+const arrowgreet=()=>{
+    console.log('hello!')
+}
+arrowgreet()
